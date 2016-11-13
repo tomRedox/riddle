b078d1b29553b39061e7a10627d070f755d7ab2b
Tom Woodforde <tom.woodforde@redox-software.co.uk>
2016-11-13 12:03:20 +0000

    added payable keyword

--- try3.js
+++ try3.js
@@ -119,7 +119,7 @@ web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 //var balance = web3.eth.getBalance(coinbase);
 //console.log(balance.toString(10));
 
-var contractAddress = "0x6149D50AEde716aecac99FBECA8736e8974d8879";
+var contractAddress = "0xF9058D9B0a6b51eF42829567AA82E1ebcb9F5737"; //"0x6149D50AEde716aecac99FBECA8736e8974d8879";
 
 
 
@@ -174,16 +174,16 @@ console.log(result);
 
 
 var result1 = contract.get_reward.call();
-console.log(result);
+console.log(result1);
 
 var tx = contract.set_question.sendTransaction(
     "so long and",
     "thanks for all the fish",
     {
-        value: 15,
-        to: contractAddress,
-        from: web3.eth.accounts[0],
-        gas: 2000000,
+        //value: 15,
+        //to: contractAddress,
+        from: web3.eth.accounts[0],//"0x9A436B996Ae7fE9398Add9DDFc0d5e60ba8A5056",//
+        //gas: 2000000,
         // data: [ "so long and",
         //   "thanks for all the fish"]
     },
@@ -199,9 +199,9 @@ var tx = contract.set_question.sendTransaction(
 console.log("Transaction ID is ",tx);
 
 var result2 = contract.get_reward.call();
-console.log(result);
+console.log(result2);
 
 var result3 = contract.get_question.call();
-console.log(result);
+console.log(result3);
 
 console.log("finished");
